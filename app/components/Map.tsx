005c39c06aab4de52a601d03b011add9c39cc226
Marcelo Rodriguez <mar.rp89@gmail.com>
2026-05-19 01:27:56 -0400

    modifico el mapa

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -14,7 +14,7 @@ export default function Map(){
             className="max-w-6xl mx-auto px-6 py-6"
         >
             <h2 className="text-primary py-6">Mapa de Toltén</h2>
-            <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1wYC2pIXny1DT_KslHigH4xVYRgfLRjM&ehbc=2E312F" 
+            <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1wYC2pIXny1DT_KslHigH4xVYRgfLRjM&ehbc=2E312F&noprof=1" 
                     width="100%" 
                     height="500"
                     style={{border: "none"}}
